928083c9759b6c42260a59042a49da74b1953a1b
kyle hoernlein <khoernlein90@gmail.com>
2017-10-17 01:32:48 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    port: 8889,
+    port: 3306,
     database: "bamazon"
 });
 
@@ -23,7 +23,6 @@ function afterConnection() {
         for (var i = 0; i < res.length; i++) {
             console.log("----------------------")
             console.log(res[i].id + ". " + res[i].product_name + " ---- $" + res[i].price);
-
         }
         inquirer.prompt([{
             name: "product",
@@ -32,18 +31,51 @@ function afterConnection() {
             choices: ["Yes", "No"]
         }]).then(function(answer) {
             if (answer.product === "Yes") {
-                for (var i = 0; i < res.length; i++) {
-            
                 inquirer.prompt([{
-                    name: "item",
-                    message: "Which item?",
+                    name: "choice",
                     type: "list",
-                    choices: [res[i].product_name]
-                }])
+                    choices: function() {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].product_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which item would you like to purchase?"
+                }, {
+                    name: "amount",
+                    type: "input",
+                    message: "How many would you like to purchase?",
+                    validate: function(value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }]).then(function(item) {
+                    var chosenItem;
+                    var newQuantity = 0;
+                    for (var i = 0; i < res.length; i++) {
+                        if (res[i].product_name === item.choice) {
+                            chosenItem = res[i];
+                        }
+                    }
+                    if (chosenItem.stock_quantity >= item.amount) {
+                        newQuantity = chosenItem.stock_quantity - item.amount;
+                        connection.query("UPDATE products SET ? WHERE ?", [{
+                            stock_quantity: newQuantity
+                        }, {
+                            id: chosenItem.id
+                        }])
+                    } else {
+                        console.log("Insufficient Quantity! There are only " + chosenItem.stock_quantity + " " + chosenItem.product_name + " left!")
+                    }
+                })
             }
-        }
+            if (answer.product === "No") {
+                console.log("Give me your money!")
+                connection.end()
+            }
         })
-        connection.end();
     })
-
 }
